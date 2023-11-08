091a1a1c9665b155be753b200fe3fae7863e0410
Olubusolami Sogunle <olubusolamisogunle@Olubusolamis-MacBook-Pro.local>
2023-11-08 23:29:11 +0100

    implement yargs #1

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const fs= require('./notes.js')
+// const fs= require('./notes.js')
+const chalk = require('chalk')
+const yargs=require('yargs')
 
-const txt= fs();
+//customize yargs version
+yargs.version('1.1.0')
 
-console.log(txt)
+
+//create add command
+yargs.command({
+    command:'add',
+    describe: 'add a new note',
+    handler: function(){
+        console.log('adding a new note!')
+    }
+})
+
+//create remove command
+yargs.command({
+    command:'remove',
+    describe: 'remove a note',
+    handler: function(){
+        console.log('removing a note!')
+    }
+})
+
+//create list command
+yargs.command({
+    command:'list',
+    describe: 'list all notes',
+    handler: function(){
+        console.log('here is the list of notes!')
+    }
+})
+
+//create read command
+yargs.command({
+    command:'read',
+    describe: 'read note',
+    handler: function(){
+        console.log('content of note displayed')
+    }
+})
+
+console.log(yargs.argv)
